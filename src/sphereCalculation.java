4358848502aa558219953fba83502a56c8aab497
Brandon Berney <brandonberney@protonmail.com>
2015-08-26 13:16:08 -0400

    ADDED THE SUDO CODE!

---
+++ src/sphereCalculation.java
@@ -0,0 +1,19 @@
+/**
+ * Author: Brandon B.
+ * Date: 8-26-15
+ */
+ 
+public class sphereCalculation {
+    
+    public static void main(String[] args) {
+    	// Read radius
+    	// Calculate the diameter
+    	// Calculate cirCUMference (-;
+    	// Calculate surface area
+    	// Calculate volume
+    	// Print diameter
+    	// Print circumference
+    	// Print surface area
+    	// Print volume
+    }
+}
